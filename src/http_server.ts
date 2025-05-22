fc4a23b321807680b0bdb7640c5859e5900d9fb2
Jethro <30698209+jaywayawyaj@users.noreply.github.com>
2025-05-22 15:03:24 +0100

    Add headers

--- src/http_server.ts
+++ src/http_server.ts
@@ -1,9 +1,19 @@
 import * as http from "http";
 
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+    const url: string | undefined = req.url;
     const headers: http.OutgoingHttpHeaders = { 'Content-Type': 'text/plain' };
-    res.writeHead(200, headers)
-    res.write('<h1>Welcome to my TypeScript Server!</h1>');
+
+    if (url === '/') {
+        res.writeHead(200, headers)
+        res.write('<h1>Welcome to the home page!</h1>');
+    } else if (url === '/about') {
+        res.writeHead(200, headers)
+        res.write('<h1>About page</h1><p>This is the about page</p>')
+    } else {
+        res.writeHead(404, headers);
+        res.write('<h1>404 Not Found</h1><p>The page you requested does not exist.</p>');
+    }
     res.end();
 })
 
