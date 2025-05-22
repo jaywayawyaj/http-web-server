17ff80320ae4695550cacd3d033eafc4d17bbcc2
Jethro <30698209+jaywayawyaj@users.noreply.github.com>
2025-05-22 15:52:23 +0100

    Serve static files

--- src/http_server.ts
+++ src/http_server.ts
@@ -1,20 +1,26 @@
 import * as http from "http";
+import * as fs from 'fs/promises';
 
-const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+const server: http.Server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const url: string | undefined = req.url;
-    const headers: http.OutgoingHttpHeaders = { 'Content-Type': 'text/plain' };
+    const headers: http.OutgoingHttpHeaders = { 'Content-Type': 'text/html' };
 
     if (url === '/') {
-        res.writeHead(200, headers)
-        res.write('<h1>Welcome to the home page!</h1>');
-    } else if (url === '/about') {
-        res.writeHead(200, headers)
-        res.write('<h1>About page</h1><p>This is the about page</p>')
+        try {
+            const data: string = await fs.readFile('index.html', 'utf8')
+            res.writeHead(200, headers)
+            res.write(data);
+            res.end();
+    } catch (err: unknown) {
+            res.writeHead(500, headers);
+            res.write('<h1>500 Internal Server Error</h1><p>Could not read file.</p>');
+            res.end();
+        }
     } else {
         res.writeHead(404, headers);
         res.write('<h1>404 Not Found</h1><p>The page you requested does not exist.</p>');
+        res.end();
     }
-    res.end();
 })
 
 server.listen(3000, () => {
