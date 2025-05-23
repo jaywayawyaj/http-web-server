238ad189293c5d8f516c5ebef199202f539ef326
Jethro <30698209+jaywayawyaj@users.noreply.github.com>
2025-05-23 14:53:53 +0100

    Add post/submit

--- src/http_server.ts
+++ src/http_server.ts
@@ -3,9 +3,10 @@ import * as fs from 'fs/promises';
 
 const server: http.Server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const url: string | undefined = req.url;
+    const method: string = req.method || "GET";
     const headers: http.OutgoingHttpHeaders = { 'Content-Type': 'text/html' };
 
-    if (url === '/') {
+    if (url === '/' && method === 'GET') {
         try {
             const data: string = await fs.readFile('index.html', 'utf8')
             res.writeHead(200, headers)
@@ -27,7 +28,17 @@ const server: http.Server = http.createServer(async (req: http.IncomingMessage, 
           res.write('<h1>500 Server Error</h1><p>Could not read file.</p>');
           res.end();
         }
-      } else {
+      } else if (url === '/submit' && method === 'POST') {
+        let body: string = '';
+        req.on('data', (chunk: Buffer) => {
+            body += chunk.toString();
+        })
+        req.on('end', () => {
+            res.writeHead(200, headers);
+            res.write(`<h1>Received POST Data</h1><p>Data: ${body}</p>`);
+            res.end();
+        })
+      }else {
         res.writeHead(404, headers);
         res.write('<h1>404 Not Found</h1><p>The page you requested does not exist.</p>');
         res.end();
